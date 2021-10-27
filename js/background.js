bf964131fad586af2b6f1c7366da8e3b881cee57
vancer7 <vance@vancer.net>
2021-10-27 10:10:35 -0500

    Removed Unused code

--- js/background.js
+++ js/background.js
@@ -1,14 +1,14 @@
-var scroller_on = false;
-var scroller;
-var speed;
-chrome.runtime.onInstalled.addListener(function () {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: { hostEquals: 'www.cvs.com' },
-			})],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
-});
+//var scroller_on = false;
+//var scroller;
+//var speed;
+//chrome.runtime.onInstalled.addListener(function () {
+//	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//		chrome.declarativeContent.onPageChanged.addRules([{
+//			conditions: [new chrome.declarativeContent.PageStateMatcher({
+//				pageUrl: { hostEquals: 'www.cvs.com' },
+//			})],
+//			actions: [new chrome.declarativeContent.ShowPageAction()]
+//		}]);
+//	});
+//});
 
